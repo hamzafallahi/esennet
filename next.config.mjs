0991ab3132ab23b848ef2963a775ca749e173877
Hamza Fallahi <131078775+hamzafallahi@users.noreply.github.com>
2024-11-16 00:48:02 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "assets.aceternity.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
